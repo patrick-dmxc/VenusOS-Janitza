35b6c434ef51f54e04a5e25732f3574eb10ec6a8
pgrote <patrick.grote@dmxcontrol-projects.org>
2026-06-18 20:44:35 +0200

    grr

--- Janitza.py
+++ Janitza.py
@@ -343,12 +343,12 @@ class JANITZA_UMG_801_BASIC_GROUP(device.SubDevice):
     age_limit_fast = 0
     refresh_time = 200
     nr_phases = 3
-    # role_names = ['grid', 'pvinverter', 'genset', 'acload', 'evcharger',
-    #               'heatpump']
-    # allowed_roles = role_names
-    # default_role = 'grid'
-    # default_instance = 41
-    # position = None
+    role_names = ['grid', 'pvinverter', 'genset', 'acload', 'evcharger',
+                  'heatpump']
+    allowed_roles = role_names
+    default_role = 'grid'
+    default_instance = 41
+    position = None
 
 
     def __init__(self, parent, basic_group_num):
