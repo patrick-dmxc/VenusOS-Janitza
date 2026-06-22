eee7611a0fab45669f609262ca499b8e5d98de9b
pgrote <patrick.grote@dmxcontrol-projects.org>
2026-06-22 22:35:30 +0200

    revert

--- Janitza.py
+++ Janitza.py
@@ -427,7 +427,7 @@ class JANITZA_UMG_801_BASIC_GROUP(device.CustomName, device.SubDevice):
                 Reg_f32b(voltageLineToLineAddress, '/Ac/L%d/VoltageLineToLine' % n, 1, '%.3f V'),
                 Reg_f32b(currentAddress,           '/Ac/L%d/Current' % n,           1, '%.3f A'),
                 Reg_f32b(activePowerAddress,       '/Ac/L%d/Power' % n,             1, '%.3f W'),
-                Reg_f32b(apparentPowerAddress,     '/Ac/L%d/ApparentPower' % n,     1, '%.3f VA'),
+                # Reg_f32b(apparentPowerAddress,     '/Ac/L%d/ApparentPower' % n,     1, '%.3f VA'),
                 # Reg_f32b(reactivePowerAddress,     '/Ac/L%d/ReactivePower' % n,     1, '%.3f var'),
                 Reg_f32b(energyFwdAddress,         '/Ac/L%d/Energy/Forward' % n, 1000, '%.3f kWh'),
                 Reg_f32b(energyRevAddress,         '/Ac/L%d/Energy/Reverse' % n, 1000, '%.3f kWh'),
@@ -479,7 +479,7 @@ class JANITZA_UMG_801_BASIC_GROUP(device.CustomName, device.SubDevice):
         try:
             gRegs = [
                 Reg_f32b(activePowerAddress, '/Ac/Power',             1, '%.3f W'),
-                Reg_f32b(apparentPowerAddress, '/Ac/ApparentPower',   1, '%.3f VA'),
+                # Reg_f32b(apparentPowerAddress, '/Ac/ApparentPower',   1, '%.3f VA'),
                 # Reg_f32b(reactivePowerAddress, '/Ac/ReactivePower',   1, '%.3f var'),
                 Reg_f32b(currentAddress,     '/Ac/Current',           1, '%.3f A'),
                 Reg_f32b(frequencyAddress,   '/Ac/Frequency',         1, '%.3f Hz'),
